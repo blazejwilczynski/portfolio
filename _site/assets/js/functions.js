88f272960a5b8b70c5d7e30f7db87083427ab9ff
Blazej <32243916+blazejwilczynski@users.noreply.github.com>
2020-12-27 17:49:14 +0100

    cleanout of function.js file

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -18,31 +18,3 @@ function smoothScrool(duration) {
   });
 }
 
-buttonRedCircle = document.getElementById("red-circle");
-buttonRedCircle.addEventListener("click", function () {
-  console.log(`Red button click`);
-  bodyGrab = document.querySelector("body");
-  console.log(bodyGrab);
-  bodyGrab.style.display = "none";
-});
-
-const slogan1Changer = document.getElementById("slogan-1-changer");
-let counter = 0;
-
-function change() {
-  const sloganOneWords = [
-    "websites",
-    "html/css",
-    "javascript",
-    "uxui",
-    "design",
-    "writing",
-    "websites",
-  ];
-  slogan1Changer.textContent = sloganOneWords[counter];
-  counter++;
-  if (counter >= sloganOneWords.length) {
-    counter = 0;
-  }
-}
-setInterval(change, 3500);
